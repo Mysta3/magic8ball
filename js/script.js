d47f4e703fa35afda057250eda6ab063d9bf1155
Mysta3 <myshawnestallings@gmail.com>
2019-06-19 13:33:01 -0700

    added js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    var magic8Ball = {
+        listOfAnswers : ["yes", "no", "maybe", "It is certain", "I mean...if you want to", "why not?", "YOLO", "YAAAAS", "Uhhhh no..., YOU REALLY THINK THAT'S SMART", "YOU ARE BETTER", "STOP"],
+    
+        answer : function (question){
+    $("#8ball").effect( "shake" ); 	
+    $("#answer").fadeIn(4000);
+    
+    var randomNumber = Math.random();
+    var randomNumberArray = randomNumber * 		 		this.listOfAnswers.length;
+    var randomIndex = Math.floor(randomNumberArray);
+    
+        var randomAnswer = 	this.listOfAnswers[randomIndex];
+    $("#answer").text( randomAnswer );
+    console.log(randomAnswer);
+    
+    }
+    }; 
+    
+    $("#answer").hide();
+    
+    var onClick = function() {
+    $("#answer").hide();
+    $("#8ball").attr("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+        
+    setTimeout( function(){
+    var question = prompt("Ask me a question?");
+    magic8Ball.answer(question);
+    },500);
+    };
+    
+    $("#questionButton").click( onClick );
+    });

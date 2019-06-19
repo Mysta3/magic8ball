09344ce551eb4aef33e3ce23afeeebe17faa8bcc
Mysta3 <myshawnestallings@gmail.com>
2019-06-19 13:40:15 -0700

    added js

--- js/script.js
+++ js/script.js
@@ -21,12 +21,17 @@ $(document).ready(function(){
     
     var onClick = function() {
     $("#answer").hide();
+
+    setTimeout( function(){
+    $("#8ball").fadeIn(3000);
     $("#8ball").attr("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
-        
+    
+    }, 2000);
+
     setTimeout( function(){
     var question = prompt("Ask me a question?");
     magic8Ball.answer(question);
-    },500);
+    },1000);
     };
     
     $("#questionButton").click( onClick );
